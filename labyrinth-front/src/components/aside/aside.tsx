815fa8df677ba546372efe3e4999a0e8130bfcd4
Daniil Ponomarev <daniiil.ponomarev@gmail.com>
2020-01-29 15:43:48 +0300

    Improve aside

--- labyrinth-front/src/components/aside/aside.tsx
+++ labyrinth-front/src/components/aside/aside.tsx
@@ -1,12 +1,30 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { IconButton, List, SwipeableDrawer, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { List, SwipeableDrawer, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import MailIcon from '@material-ui/icons/Mail';
 
-import { LogoSVG, InnerPath, OuterPath } from 'components';
+import { colors } from 'utils';
 
-const StyledLogoSVG = styled(LogoSVG)`
-  margin-right: 1rem;
+const SideListWrapper = styled.div`
+  display: grid;
+  grid-template: 4rem 1fr 3rem / 100%;
+  height: 100%;
+`;
+
+const HeaderPart = styled.div`
+  height: 4rem;
+  background-color: ${colors.gray0};
+  box-shadow: 0 2px 2px 3px ${colors.gray20};
+`;
+
+const StyledSideList = styled.div`
+  width: 25rem;
+`;
+
+const FooterPart = styled.div`
+  height: 3rem;
+  background-color: ${colors.gray0};
+  box-shadow: 0 -2px 2px 3px ${colors.gray20};
 `;
 
 export const Aside: React.FC<any> = (props: { isMenuOpened: boolean }) => {
@@ -31,24 +49,24 @@ export const Aside: React.FC<any> = (props: { isMenuOpened: boolean }) => {
     setIsMenuOpened(isMenuOpened);
   };
 
-  const SideList = () => (
-    <div role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
-      <List>
-        {['New Game', 'Feedback', 'About'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              <MailIcon />
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   return (
     <SwipeableDrawer open={isMenuOpened} onClose={toggleDrawer(false)} onOpen={toggleDrawer(true)}>
-      <SideList />
+      <SideListWrapper>
+        <HeaderPart />
+        <StyledSideList role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)}>
+          <List>
+            {['New Game', 'Feedback', 'About'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  <MailIcon />
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </StyledSideList>{' '}
+        <FooterPart />
+      </SideListWrapper>
     </SwipeableDrawer>
   );
 };
