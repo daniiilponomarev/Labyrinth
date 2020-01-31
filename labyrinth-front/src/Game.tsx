9ce332a6e7ac355ce5558086a94e0d900c67b93d
Daniil Ponomarev <daniiil.ponomarev@gmail.com>
2020-01-31 12:15:40 +0300

    Add font

--- labyrinth-front/src/Game.tsx
+++ labyrinth-front/src/Game.tsx
@@ -17,7 +17,7 @@ html {
 
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+  font-family: 'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
     'Droid Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -72,8 +72,21 @@ const muiTheme = createMuiTheme({
   },
   typography: {
     fontSize: 12,
-    htmlFontSize: 10
+    htmlFontSize: 10,
     // useNextVariants: true,
+    fontFamily: [
+      'Lato',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(',')
   }
 });
 
